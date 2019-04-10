87b13fc6e134cb8dbc080524a7218f17eeab3176
spikeeSakshu <spikee.sakshu@gmail.com>
2019-04-10 13:33:56 +0530

    Using BayesianRidge

--- kernel.py
+++ kernel.py
@@ -4,28 +4,20 @@ Created on Tue Apr  9 19:22:42 2019
 
 @author: Spikee
 """
+import numpy as np 
+import pandas as pd
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-data = pd.read_csv('../input/diabetes/pima-indians-diabetes.csv')
-data.head()
+data = pd.read_csv('pima-indians-diabetes.csv')
 
 data['Skin'].replace(0, np.nan, inplace=True)
-data.head()
 
 data_without_missing_values = data.dropna()
-data_without_missing_values.head()
 
 data_with_missing_values = data[data.isnull().any(axis=1)]
-data_with_missing_values.head()
 
-misc = ['Skin', 'Label']
-X_train = data_without_missing_values.drop(misc, axis=1)
-X_train.head()
+X_train = data_without_missing_values.drop(['Skin'], axis=1)
 
 y_train = data_without_missing_values['Skin']
-y_train.head()
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size = 0.25, random_state = 10)
@@ -36,9 +28,49 @@ model = BayesianRidge(compute_score = True, fit_intercept= True, tol= 10)
 model = model.fit(X_train, y_train)
 
 print("Results For Bayesian Ridge")
-score = model.score(X_test, y_test)
-print("\nScore", score*100)
+scoreBeforeFilling = model.score(X_test, y_test)
+print("\nScore", scoreBeforeFilling*100)
 
+X_test_nan = data_with_missing_values.drop(['Skin'], axis=1)
+y_test_nan = data_with_missing_values['Skin']
+
+predicted = model.predict(X_test_nan)
+
+predicted =predicted.astype(int)
+
+# =============================================================================
+# #
+# =============================================================================
+data_with_missing_values['Skin'] = predicted
+
+data_after_filling = pd.concat([data_without_missing_values, data_with_missing_values], ignore_index = True)
+
+from sklearn.utils import shuffle
+data_after_filling = shuffle(data_after_filling, random_state=10)
+
+X_train = data_after_filling.drop(['Skin'], axis=1)
+y_train = data_after_filling['Skin']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size = 0.25, random_state = 10)
+
+
+from sklearn.linear_model import BayesianRidge
+model = BayesianRidge(compute_score = True, fit_intercept= True, tol= 10)
+model = model.fit(X_train, y_train)
+
+print("Results For Bayesian Ridge")
+scoreAfterFilling = model.score(X_test, y_test)
+print("\nScore", scoreAfterFilling*100)
+
+
+
+
+
+
+# =============================================================================
+# 
+# =============================================================================
 from sklearn.linear_model import LinearRegression
 modelReg = LinearRegression(fit_intercept= True)
 modelReg = modelReg.fit(X_train, y_train)
